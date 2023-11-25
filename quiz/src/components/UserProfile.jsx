ebeaadef0e8e67b7c0d71dd22bcafa5cdb93db2a
julio <julioamsf@gmail.com>
2023-11-24 22:25:05 -0400

    Adicionando a funcionalidade de 'minha media' ao perfil do jogador

--- quiz/src/components/UserProfile.jsx
+++ quiz/src/components/UserProfile.jsx
@@ -1,13 +1,26 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext';
 import Star from './svgs/Star';
 import styles from '../styles/UserProfile.module.css';
+import { getRanking } from '../services/api';
 
 const UserProfile = () => {
+  const [userMedia, setUserMedia] = useState(0);
+
   const { data, userLogout } = useContext(UserContext);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const attUserMedia = async () => {
+      const userMedia = await getRanking(data.uid);
+
+      if (userMedia) setUserMedia(userMedia.userScore);
+    };
+
+    attUserMedia();
+  }, [data]);
 
   return (
     <main className="container wrapper animationBottom">
@@ -15,13 +28,9 @@ const UserProfile = () => {
         <h4 className={styles.title}>Minha média</h4>
         <section className={styles.flex}>
           <Star type={'gradient'} />
-          <p>8/10</p>
+          <p>{userMedia.toFixed(1)}/5.0</p>
           <Star type={'gradient'} />
         </section>
-      </article>
-      <article className="articleBox">
-        <h4>Minha melhor modalidade</h4>
-        <span>{'Todos'}</span>
       </article>
       <article className="articleBox">
         <h4>Meu nome</h4>
@@ -31,7 +40,9 @@ const UserProfile = () => {
         <h4>Meu e-mail</h4>
         <span>{data.email}</span>
       </article>
-      {data && <button onClick={() => navigate('/changedata')}>Alterar dados</button>}
+      {data && (
+        <button onClick={() => navigate('/changedata')}>Alterar dados</button>
+      )}
       {data && <button onClick={userLogout}>Sair</button>}
       <button className="buttonDarkTheme" onClick={() => navigate(-1)}>
         Voltar
